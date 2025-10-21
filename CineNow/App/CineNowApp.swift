f7d93ba075bc43aa2cf92cc9119680f6f1698cb4
Mickael Belhassen <emoonadev@gmail.com>
2025-10-21 14:39:19 +0300

    Initial setup with folder structure and SPM

--- CineNow/App/CineNowApp.swift
+++ CineNow/App/CineNowApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct CineNowApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+//            Item.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
@@ -25,7 +25,7 @@ struct CineNowApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
         .modelContainer(sharedModelContainer)
     }
---
+++ CineNow/Features/Main/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CineNow
+//
+//  Created by Mickael Belhassen on 21/10/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct MainView: View {
+
+    var body: some View {
+        Text("Hello, world!")
+    }
+
+}
+
+#Preview {
+    MainView()
+}
