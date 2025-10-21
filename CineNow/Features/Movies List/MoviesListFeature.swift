d3d94c19ad37f8fd5504b43b3dfd3fff83812010
Mickael Belhassen <emoonadev@gmail.com>
2025-10-21 14:45:20 +0300

    Feature skeletons added

---
+++ CineNow/Features/Movies List/MoviesListFeature.swift
@@ -0,0 +1,14 @@
+//
+//  MoviesListFeature.swift
+//  CineNow
+//
+//  Created by Mickael Belhassen on 21/10/2025.
+//
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct MoviesListFeature {
+    
+}
---
+++ CineNow/Features/Movies List/MoviesListView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CineNow
+//
+//  Created by Mickael Belhassen on 21/10/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct MoviesListView: View {
+
+    var body: some View {
+        Text("Movies List")
+    }
+
+}
+
+#Preview {
+    MoviesListView()
+}
